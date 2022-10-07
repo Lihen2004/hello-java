a64b50998c3e47be83f11e6b9c0ac254de1e84d9
Lihen2004 <114204331+Lihen2004@users.noreply.github.com>
2022-10-07 11:39:00 +0800

    10/7

--- Test7.java
+++ Test7.java
@@ -2,11 +2,14 @@ import java.util.Scanner;
 
 public class Test7 {
     public static void main(String[] args) {
-        int grade = 9;
+        int grade = 0;
+        String space = "";
         
-        while (grade >= 0) {
-            System.out.printf("%d %n", grade);
-            grade = grade - 1;
+        while (grade < 10) {
+            System.out.printf("%s", space);
+            System.out.printf("%d%n", grade);
+            grade = grade + 1;
+            space = space + " ";
         }
     }
 }
