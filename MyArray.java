109f2793bbe545cb817e7571f66236fa5cea65cd
Lihen2004 <114204331+Lihen2004@users.noreply.github.com>
2023-02-24 10:56:36 +0800

    2/24

---
+++ MyArray.java
@@ -0,0 +1,19 @@
+public class MyArray {
+    public static void main(String[] args) {
+        Human tom = new Human();
+    }
+}
+
+class Human {
+    int sex;
+    int height = 180;
+    private int weight = 75;
+    
+    public void printHight(){
+        System.out.println(height);  
+    }
+    
+    public void printHight(){
+        System.out.println(weight);
+    }
+}
